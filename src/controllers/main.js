011fa31b01955ac55d39f6e139f8ae8b0342035c
Isaiah <pictureisaiah@gmail.com>
2021-06-27 12:51:55 -0400

    Creation of backend concerning Postgres database

---
+++ src/controllers/main.js
@@ -0,0 +1,50 @@
+
+const getTableData = (req, res, db) => {
+    db.select('*').from('shoe')
+      .then(items => {
+        if(items.length){
+          res.json(items)
+        } else {
+          res.json({dataExists: 'false'})
+        }
+      })
+      .catch(err => res.status(400).json({dbError: 'db error'}))
+  }
+
+  // Insert to database 
+  const postTableData = (req, res, db) => {
+    const { id, name, model, price, size, color, description, rating} = req.body
+    db('shoe').insert({id, name, model, price, size, color, description, rating})
+      .returning('*')
+      .then(item => {
+        res.json(item)
+      })
+      .catch(err => res.status(400).json({dbError: 'db error'}))
+  }
+  
+  // Replace data
+  const putTableData = (req, res, db) => {
+    const { id, name, model, size, price, color, description, rating } = req.body
+    db('shoe').where({id}).update({id, name, model, size, price, color, description, rating})
+      .returning('*')
+      .then(item => {
+        res.json(item)
+      })
+      .catch(err => res.status(400).json({dbError: 'db error'}))
+  }
+  
+  const deleteTableData = (req, res, db) => {
+    const { id } = req.body
+    db('shoe').where({id}).del()
+      .then(() => {
+        res.json({delete: 'true'})
+      })
+      .catch(err => res.status(400).json({dbError: 'db error'}))
+  }
+  
+  module.exports = {
+    getTableData,
+    postTableData,
+    putTableData,
+    deleteTableData
+  }
