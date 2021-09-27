1257a33a944be24005af5a04f21e6a3b8aa43569
Isaiah <pictureisaiah@gmail.com>
2021-09-26 23:53:00 -0400

    code cleaning

--- src/Home.js
+++ src/Home.js
@@ -129,12 +129,8 @@ const Home = props => {
                   />     
                 </div>
             </div>
-            <div className="intro column is-8 is-offset-2">
-                <h2 className="title">Perfect for developers or designers!</h2><br />
-                <p className="subtitle">Vel fringilla est ullamcorper eget nulla facilisi. Nulla facilisi nullam vehicula ipsum a. Neque egestas congue quisque egestas diam in arcu cursus.</p>
-            </div>
             <div className="section">
-            <div className="d-flex flex-row flex-wrap ">
+            <div className="d-flex flex-row flex-wrap">
                   { products.map((product) => (
                     <div
                       key={product.id}
